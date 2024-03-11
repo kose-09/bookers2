e2935ac39465afb0ca22ab4cacd77511a72b01f9
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-03-11 16:05:58 +0000

    [Add] bookの更新機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books
   resources :users, only: [:show, :edit, :index]
 
   get 'homes/about' => 'homes#about', as: 'about'
