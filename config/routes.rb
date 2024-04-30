35202efd30bb8f744d22424509c03c09562e62af
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-04-30 14:18:28 +0000

    [Add] ゲストログインの機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   resources :books, only: [:show, :index, :edit, :update, :destroy, :create]
   resources :users, only: [:show, :edit, :update, :index]
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 
   get 'home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
