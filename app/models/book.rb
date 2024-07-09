5822270b14fe7bf29c658857e3e012598e004440
KoseiFukumi <158475203+kose-09@users.noreply.github.com>
2024-07-09 15:54:25 +0000

    [Add] Fovoriteモデルの作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :notifications, as: :notifiable, dependent: :destroy
 
   validates :title, :body, presence: true
   validates :body, length: { maximum: 200 }
